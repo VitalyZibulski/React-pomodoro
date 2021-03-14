42a0b5fcb3c385ba2574bdf41186e51bc8ca5301
VitalyZibulski <laravel.vitaly.zibulski2017@gmail.com>
2021-03-14 20:55:24 +0300

    peoject

--- src/App.js
+++ src/App.js
@@ -1,21 +1,63 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import './App.css';
+import { padTime } from "./utils/utils";
 
 export default function App() {
+  const [title, setTitle] = useState('Let the countdown begin');
+  const [timeLeft, setTimeleft] = useState(25 * 60);
+  const minutes = padTime(Math.floor(timeLeft / 60));
+  const seconds = padTime(timeLeft - minutes * 60);
+  const [isRunning, setIsRunning] = useState(false);
+  const intervalRef = useRef(null);
+
+  const startTimer = () => {
+    if (intervalRef.current !== null) return;
+
+    setTitle(`You're doing great`);
+    setIsRunning(true);
+    intervalRef.current = setInterval(() => {
+        setTimeleft(timeLeft => {
+            if (timeLeft >= 1) {
+              return timeLeft -1;
+            }
+
+            resetTimer();
+            return 0;
+        });
+      }, 1000);
+  }
+
+  const stopTimer = () => {
+    if (intervalRef.current === null) return;
+
+    clearInterval(intervalRef.current);
+    intervalRef.current = null;
+    setTitle('Keep it up');
+    setIsRunning(false);
+  }
+
+  const resetTimer = () => {
+    clearInterval(intervalRef.current);
+    intervalRef.current = null;
+    setTitle('Ready to go another round?');
+    setTimeleft(25 * 60);
+    setIsRunning(false);
+  }
+
   return (
     <div className="app">
-      <h2>Pomodoro!</h2>
+      <h2>{title}</h2>
 
       <div className="timer">
-        <span>00</span>
+        <span>{minutes}</span>
         <span>:</span>
-        <span>00</span>
+        <span>{seconds}</span>
       </div>
 
       <div className="buttons">
-        <button>Start</button>
-        <button>Stop</button>
-        <button>Reset</button>
+        {!isRunning && <button onClick={startTimer}>Start</button>}
+        {isRunning && <button onClick={stopTimer}>Stop</button>}
+        <button onClick={resetTimer}>Reset</button>
       </div>
     </div>
   );
